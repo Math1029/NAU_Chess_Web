e37dcf5dd195f2a1ac0f7ceeb114319cc32b9f29
Evan Paddock <haastiin@protonmail.com>
2025-08-01 20:47:52 -0700

    Formatted the webpages to be 'mobile first' that compensates for the small width. As width becomes large enough, the webpages will shift to desktop view. Removed the about page as that would already be provided by True Blue Connects. Updated the tournaments table for upcoming events. The fonts on the webpages now use rem meaning that the user can adjust font size in the settings.

---
+++ shared.js
@@ -0,0 +1,20 @@
+var toggleButton = document.querySelector(".toggle-button")
+var backdrop = document.querySelector(".backdrop");
+var mobileNav = document.querySelector(".mobile-nav");
+
+function openMenu()
+{
+    backdrop.classList.add("open");
+    mobileNav.classList.add("open");
+}
+
+function closeMenu()
+{
+    backdrop.classList.remove("open");
+    mobileNav.classList.remove("open");
+
+}
+
+toggleButton.addEventListener("click", openMenu);
+
+backdrop.addEventListener("click", closeMenu);
